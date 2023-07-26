88319a89ddd5be8c6975270fd7dbe4b5231776cd
Joachim Metz <joachim.metz@gmail.com>
2023-07-26 05:52:56 +0200

    Applied updates

--- libfsfat/libfsfat_volume.c
+++ libfsfat/libfsfat_volume.c
@@ -612,9 +612,9 @@ int libfsfat_volume_open_file_io_handle(
 {
 	libfsfat_internal_volume_t *internal_volume = NULL;
 	static char *function                       = "libfsfat_volume_open_file_io_handle";
+	uint8_t file_io_handle_opened_in_library    = 0;
 	int bfio_access_flags                       = 0;
 	int file_io_handle_is_open                  = 0;
-	int file_io_handle_opened_in_library        = 0;
 
 	if( volume == NULL )
 	{
@@ -741,7 +741,7 @@ int libfsfat_volume_open_file_io_handle(
 	}
 #endif
 	internal_volume->file_io_handle                   = file_io_handle;
-	internal_volume->file_io_handle_opened_in_library = (uint8_t) file_io_handle_opened_in_library;
+	internal_volume->file_io_handle_opened_in_library = file_io_handle_opened_in_library;
 
 #if defined( HAVE_LIBFSFAT_MULTI_THREAD_SUPPORT )
 	if( libcthreads_read_write_lock_release_for_write(
