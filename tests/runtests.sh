51a870f9e05213bfb1941b705d0bd3d998c5e897
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 06:45:40 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfsfat/.libs/libfsfat.1.dylib && test -f ./pyfsfat/.libs/pyfsfat.so
+if [ -f "${PWD}/libfsfat/.libs/libfsfat.1.dylib" ] && [ -f ./pyfsfat/.libs/pyfsfat.so ]
 then
-	install_name_tool -change /usr/local/lib/libfsfat.1.dylib ${PWD}/libfsfat/.libs/libfsfat.1.dylib ./pyfsfat/.libs/pyfsfat.so
+    install_name_tool -change /usr/local/lib/libfsfat.1.dylib "${PWD}/libfsfat/.libs/libfsfat.1.dylib" ./pyfsfat/.libs/pyfsfat.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
